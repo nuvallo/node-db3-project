ede05090e02f0235c2b048ecd842a4ba4c76446e
nuvallo <nuvallo.inc@gmail.com>
2020-04-29 16:47:11 -0400

    Queires Complete

--- queries.sql
+++ queries.sql
@@ -1,9 +1,27 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT ProductName, CategoryName FROM Product 
+JOIN Category
+LIMIT 77
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT CompanyName, [Order].id
+FROM [Order]
+JOIN Shipper ON Shipper.id = [Order].ShipVia
+WHERE [Order].OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+ SELECT ProductName, Quantity 
+ FROM Product 
+ JOIN OrderDetail 
+ ON Product.Id = OrderDetail.ProductId 
+ WHERE OrderDetail.OrderId = '10251'
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT CompanyName, LastName AS EmployeesLastName, [Order].Id
+	FROM [Order]
+	    JOIN Employee
+	    ON Employee.Id = [Order].EmployeeId
+	    JOIN Customer
+	    ON Customer.Id = [Order].CustomerId
